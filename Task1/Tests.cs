426330ec2232a1b7f1734c53a87b487275a0d341
Dmitry Veresovich <Dmitry_Veresovich@epam.com>
2016-08-05 20:36:29 +0300

    Task 1 done

---
+++ Task1/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Task1
+{
+    [TestFixture]
+    class Tests
+    {
+        const string LocalDir = @"E:/Workspace/mentoring-xml/Task1/";
+
+        [Test]
+        public void Test()
+        {
+            Directory.SetCurrentDirectory(LocalDir);
+
+            var validator = new XsdValidator();
+            var errors = validator.Validate("http://library.by/catalog", "booksSchema.xsd", "../books.xml").ToList();
+            Assert.IsEmpty(errors);
+        }
+    }
+}
---
+++ Task1/XsdValidator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace Task1
+{
+    class XsdValidator
+    {
+        public IEnumerable<string> Validate(string targetNamespace, string schemaUri, string inputUri)
+        {
+            var settings = new XmlReaderSettings();
+            settings.Schemas.Add(targetNamespace, schemaUri);
+
+            var validationErrors = new List<string>();
+            settings.ValidationEventHandler += (sender, e) => validationErrors.Add(e.Message);
+
+            settings.ValidationFlags = settings.ValidationFlags | XmlSchemaValidationFlags.ReportValidationWarnings;
+            settings.ValidationType = ValidationType.Schema;
+
+            using (var reader = XmlReader.Create(inputUri, settings))
+            {
+                while (reader.Read()) ;
+            }
+
+            return validationErrors;
+        }
+    }
+}
